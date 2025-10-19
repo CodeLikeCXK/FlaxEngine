26793459b1c112aac037cd2cc04b19810f853acc
CodeLikeCXK <44729188+CodeLikeCXK@users.noreply.github.com>
2025-10-19 22:04:56 +0800

    Add tools for arm64 build

---
+++ BuildFlax.bat
@@ -0,0 +1,3 @@
+cd binaries/tools
+Flax.Build -log -workspace=C:\FlaxEngine -build -arch=ARM64 -platform=Windows -configuration=Development -buildTargets=FlaxEditor -winCpuArch=NEON
+pause
---
+++ GenerateProjectFiles -arm64.bat
@@ -0,0 +1,31 @@
+@echo off
+:: Copyright (c) Wojciech Figat. All rights reserved.
+
+setlocal
+pushd
+
+echo Generating Flax Engine project files...
+
+:: Change the path to the script root
+cd /D "%~dp0"
+
+:: Run Flax.Build to generate Visual Studio solution and project files (also pass the arguments)
+call "Development\Scripts\Windows\CallBuildTool.bat" -genproject  %*
+if errorlevel 1 goto BuildToolFailed
+
+:: Build bindings for all editor configurations
+echo Building C# bindings...
+Binaries\Tools\Flax.Build.exe -build -BuildBindingsOnly -arch=arm64 -platform=Windows --buildTargets=FlaxEditor
+
+popd
+echo Done!
+exit /B 0
+
+:BuildToolFailed
+echo Flax.Build tool failed.
+pause
+goto Exit
+
+:Exit
+popd
+exit /B 1
